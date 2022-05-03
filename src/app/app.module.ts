cd8150b0a908cd104235f63d8e8c56da9e01ac54
Javier Monleón López <javiermonleonlopez@MBProPepe.local>
2022-05-03 10:48:03 +0200

    angular project kickoff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+// import { HeaderComponent } from './components/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    // HeaderComponent,
+],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule
   ],
