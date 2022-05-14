86a7ae8d0f846f1bdbab9a28a4584ea646e6b302
Javi Monleon <monleonlopezjavier@gmail.com>
2022-05-14 02:57:41 +0200

    profile and film styles improved

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,4 +2,4 @@ export const root:string = "https://image.tmdb.org/t/p/w185";
 
 export const API_KEY:string = "210d6a5dd3f16419ce349c9f1b200d6d";
 
-//deploy URL: https://dev.d340r4q4bzma2.amplifyapp.com/
+//deploy URL: https://dev.d340r4q4bzma2.amplifyapp.com
