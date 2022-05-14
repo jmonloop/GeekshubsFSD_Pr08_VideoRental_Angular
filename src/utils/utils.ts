3ee674fa58aaeb790af716a31edb6f4a1522b4f2
Javi Monleon <monleonlopezjavier@gmail.com>
2022-05-14 02:32:02 +0200

    angular.json budgets: maximum changed

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
 export const root:string = "https://image.tmdb.org/t/p/w185";
 
 export const API_KEY:string = "210d6a5dd3f16419ce349c9f1b200d6d";
+
+//deploy URL: https://dev.d340r4q4bzma2.amplifyapp.com/
